2b146fb8896c044a830731db5eaaeab9fbd836af
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-17 03:39:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, pos)
   return false if position_taken?(board, pos)
+  return true if board[pos].empty?
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
